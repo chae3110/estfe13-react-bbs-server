3b943a86a1bab056c3236ac307b93e40e134f171
chae <dldmd33333@gmail.com>
2026-07-02 13:42:05 +0900

    feat: "수정"

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express');
-const app = express()
-const port = 3000
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var corsOptions = {
+  origin: '*' // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
