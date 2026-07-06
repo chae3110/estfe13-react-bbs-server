d0a0fa880adcabf2bfd91ba5c2b9696524f2c1ba
chae <dldmd33333@gmail.com>
2026-07-06 16:53:48 +0900

    feat:  "추가"

--- index.js
+++ index.js
@@ -1,15 +1,113 @@
 const express = require("express");
-var cors = require("cors");
+const cors = require("cors");
 const app = express();
+const mysql = require("mysql2");
 const port = 3000;
+const multer = require("multer");
 
-var corsOptions = {
-  origin: '*' // some legacy browsers (IE11, various SmartTVs) choke on 204
+app.use(express.json()); //json->object
+app.use(express.urlencoded({ extended: true })); //html form ->object
+app.use("/uploads", express.static("uploads"));
+//   /uploads 주소로 접속시 upload 폴더에 접근 권한 부여
+
+let corsOptions = {
+  origin: "*",
 };
 
+app.use(cors(corsOptions));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const orinalExt = file.originalname.split(".")[1];
+    const uniquePrefix = Date.now() + "-" + Math.round(Math.random() * 1000);
+    cb(null, uniquePrefix + "-" + file.fieldname + "." + orinalExt);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "12345",
+  database: "bbs",
+});
+
+db.connect();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/list", (req, res) => {
+  const sqlQuery =
+    "SELECT id, title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board;";
+  db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.get("/view", (req, res) => {
+  console.log(req.query.id);
+  const id = req.query.id;
+  // const sqlQuery = `SELECT * FROM board WHERE id=${req.query.id};`;
+  const sqlQuery =
+    "SELECT title, content, writer, image_path, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board WHERE id=?;";
+  db.query(sqlQuery, [id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.post("/write", upload.single("image"), (req, res) => {
+  console.log(req.body);
+  const { title, writer, content } = req.body;
+  const imagePath = req.file ? req.file.path : null; //req.file.path는 업로드된 파일의 경로
+
+  const sqlQuery = "insert into board (title,content,writer,image_path) values (?,?,?,?);";
+  db.query(sqlQuery, [title, content, writer, imagePath], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.post("/delete", (req, res) => {
+  console.log(req.body);
+  const { id } = req.body;
+
+  const sqlQuery = "DELETE FROM board WHERE id=?";
+  db.query(sqlQuery, [id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.post("/deleteselect", (req, res) => {
+  console.log(req.body);
+  const { boardIdList } = req.body;
+
+  const sqlQuery = `delete from board where id in (${boardIdList})`;
+  db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.post("/update", (req, res) => {
+  console.log(req.body);
+  const { name, title, content, id } = req.body;
+
+  let sqlQuery;
+  let params;
+
+  db.query(sqlQuery, [name, title, content, id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
 });
 
 app.listen(port, () => {
